943937e258e618c4164e6c35fcd4dac397e9e055
Gerrit Halfmann <gerrit@gerrithalfmann.com>
2023-10-19 20:42:24 +0200

    updates for landing page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,20 @@
-/** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		colors: {
+			gray: colors.neutral,
+			white: '#fff',
+		},
+		container: {
+			center: true,
+			padding: '2rem'
+		},
+		backgroundImage: {
+			'travelr': 'radial-gradient(145% 145% at 100% 0%, rgba(105, 29, 231, 0.59) 0%, rgba(105, 29, 231, 0.00) 55%);',
+			'decoder': 'radial-gradient(145% 145% at 100% 0%, rgba(255, 22, 106, 0.50) 0%, rgba(255, 22, 106, 0.00) 55%);',
+		},
 	},
 	plugins: [],
 }
