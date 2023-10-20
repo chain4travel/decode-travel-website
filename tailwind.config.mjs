cd6b27c62e8a39fd56b707ca3faacf2d1c2acb09
Gerrit Halfmann <gerrit@gerrithalfmann.com>
2023-10-20 13:05:22 +0200

    more updates

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,14 +6,18 @@ export default {
 		colors: {
 			gray: colors.neutral,
 			white: '#fff',
+			primary: '#00F3B1',
+			company: '#691DE7',
+			decoder: '#FF166A'
 		},
 		container: {
 			center: true,
 			padding: '2rem'
 		},
 		backgroundImage: {
-			'travelr': 'radial-gradient(145% 145% at 100% 0%, rgba(105, 29, 231, 0.59) 0%, rgba(105, 29, 231, 0.00) 55%);',
-			'decoder': 'radial-gradient(145% 145% at 100% 0%, rgba(255, 22, 106, 0.50) 0%, rgba(255, 22, 106, 0.00) 55%);',
+			'gradient-primary': 'radial-gradient(150% 150% at 100% 0%, rgba(255, 255, 255, 0.10) 0%, rgba(255, 255, 255, 0) 55%);',
+			'gradient-company': 'radial-gradient(145% 145% at 100% 0%, rgba(105, 29, 231, 0.59) 0%, rgba(105, 29, 231, 0.00) 55%);',
+			'gradient-decoder': 'radial-gradient(145% 145% at 100% 0%, rgba(255, 22, 106, 0.50) 0%, rgba(255, 22, 106, 0.00) 55%);',
 		},
 	},
 	plugins: [],
