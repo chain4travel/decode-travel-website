ed67defbc7523e2bc65485209710b74c5eb67004
Gerrit Halfmann <gerrit@gerrithalfmann.com>
2023-10-23 11:57:37 +0200

    frontend / layout fixes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,18 +3,19 @@ const colors = require('tailwindcss/colors')
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		colors: {
-			gray: colors.neutral,
-			white: '#fff',
-			primary: '#00F3B1',
-			company: '#691DE7',
-			decoder: '#FF166A'
-		},
 		container: {
 			center: true,
 			padding: '1rem'
 		},
 		extend: {
+			colors: {
+				gray: colors.neutral,
+				white: '#fff',
+				primary: '#00F3B1',
+				secondary: '#C38CFF',
+				company: '#691DE7',
+				decoder: '#FF166A'
+			},
 			backgroundImage: {
 				'gradient-primary': 'radial-gradient(150% 150% at 100% 0%, rgba(255, 255, 255, 0.10) 0%, rgba(255, 255, 255, 0) 55%);',
 				'gradient-company': 'radial-gradient(145% 145% at 100% 0%, rgba(105, 29, 231, 0.59) 0%, rgba(105, 29, 231, 0.00) 55%);',
