4c5a96931f0533cd95bfd370cc76035431394bdf
Gerrit Halfmann <gerrit@gerrithalfmann.com>
2023-10-25 11:25:21 +0200

    content and layout updates

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -19,6 +19,7 @@ export default {
 			backgroundImage: {
 				'gradient-primary': 'radial-gradient(150% 150% at 100% 0%, rgba(255, 255, 255, 0.07) 0%, rgba(255, 255, 255, 0) 55%);',
 				'gradient-hackerz': 'radial-gradient(100% 100% at 50% 0%, rgba(0, 243, 177, 0.20) 0%, rgba(0, 243, 177, 0.00) 51.56%);',
+				'gradient-hackerz-b': 'radial-gradient(100% 100% at 50% 125%, rgba(0, 243, 177, 0.10) 0%, rgba(0, 243, 177, 0.00) 60%);',
 				'gradient-company': 'radial-gradient(145% 145% at 100% 0%, rgba(105, 29, 231, 0.59) 0%, rgba(105, 29, 231, 0.00) 55%);',
 				'gradient-decoder': 'radial-gradient(145% 145% at 100% 0%, rgba(255, 22, 106, 0.50) 0%, rgba(255, 22, 106, 0.00) 55%);',
 			},
