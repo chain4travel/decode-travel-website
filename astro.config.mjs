1e1345baf3d10273ef17815514741da6cf757979
Gerrit Halfmann <gerrit@gerrithalfmann.com>
2024-02-03 09:54:31 +0100

    removed cookie package, added another

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
-import cookieconsent from "@jop-software/astro-cookieconsent";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    cookieconsent({
-      gui_options: {
-          consent_modal: {
-              layout: 'box',
-              position: 'bottom right',
-              transition: 'slide',
-              swap_buttons: false,
-              equalWeightButtons: false,
-          },
-          settings_modal: {
-              layout: 'box',
-              transition: 'slide',
-              equalWeightButtons: false,
-          }
-      }
-    }),
-  ]
+  integrations: [tailwind()]
 });
