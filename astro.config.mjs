d8a71f2d104a0193d905a5317b6d87fef204df34
Gerrit Halfmann <gerrit@gerrithalfmann.com>
2024-02-03 09:17:45 +0100

    Added cookie consent

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,27 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import cookieconsent from "@jop-software/astro-cookieconsent";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [
+    tailwind(),
+    cookieconsent({
+      gui_options: {
+          consent_modal: {
+              layout: 'box',
+              position: 'bottom right',
+              transition: 'slide',
+              swap_buttons: false,
+              equalWeightButtons: false,
+          },
+          settings_modal: {
+              layout: 'box',
+              transition: 'slide',
+              equalWeightButtons: false,
+          }
+      }
+    }),
+  ]
 });
