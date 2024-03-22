87d0492afef25e140feeda5460c0188cc53e7c6b
Gerrit Halfmann <gerrit@gerrithalfmann.com>
2024-03-22 09:13:51 +0100

    Added npm v18.19.1, robots, compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
+import robotsTxt from "astro-robots-txt";
+
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), robotsTxt(), compress()]
 });
