fab0105a8e08e41c61a388703f92082399fdc121
Gerrit Halfmann <gerrit@gerrithalfmann.com>
2024-03-27 11:52:13 +0100

    Removed astro-compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import robotsTxt from "astro-robots-txt";
 
-import compress from "astro-compress";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), robotsTxt(), compress()]
+  integrations: [tailwind(), robotsTxt()]
 });
